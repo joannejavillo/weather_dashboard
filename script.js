8f4405ba88a5b7636b9577732a53fb357b9593cc
Joan J <joanne.javillo@gmail.com>
2021-01-31 10:40:52 +1030

    fixing js script

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
+
 $(document).ready(function () {
 
-    // this .on("click") function will trigger the AJAX call
+    //this .on("click") fundtion will trigger the AJAX call
     $(".submitBtn").on("click", function (event) {
         event.preventDefault();
 
@@ -12,33 +13,52 @@ $(document).ready(function () {
             url: queryURL,
             method: "GET"
         }).then(function (response) {
-            console.log(response);
+            // console.log(response);
 
-            $("#temp").text("Temperature: " + response.main.temp)
-            $("#humid").text("Humidity: " + response.main.humidity)
-            $("#windSpeed").text("Wind Speed: " + response.wind.speed)
+            $("#temp").text("Temperature: " + response.main.temp + "°F")
+            $("#humid").text("Humidity: " + response.main.humidity + "%")
+            $("#windSpeed").text("Wind Speed: " + response.wind.speed + "MPH")
 
-            //UV index var
+            //weather icon
+            var weatherIcon = $("<img>");
+            weatherIcon.attr("src", "https://openweathermap.org/img/w/" + response.weather[0].icon + ".png"
+            );
+
+            $("#icon").empty();
+
+            //UV var index
 
             var lat = response.coord.lat;
             var lon = response.coord.lon;
 
-            var uvURL = `http://api.openweathermap.org/data/2.5/uvi?lat=${lat}&lon=${lon}&appid=f6fe3d0e6489f66b9ccb2d38e5cdb94b`;
+            var uvIndexURL = `http://api.openweathermap.org/data/2.5/uvi?lat=${lat}&lon=${lon}&appid=f6fe3d0e6489f66b9ccb2d38e5cdb94b`;
 
-            //$.ajax -> UV
+            //AJAX - UV
 
             $.ajax({
-                url: uvURL,
+                url: uvIndexURL,
                 method: "GET"
             }).then(function (response) {
                 console.log(response);
 
-            $("#uvIndex").text("UV Index: " + response.value)
+                $("#uvIndex").text("UV Index: " + response.value)
+
+                if (response.value > 7) {
+                    $("#indicator").removeClass();
+                    $("#indicator").addClass("indicator indicator-success");
+                }
+                else if (response.value > 3 && response.value < 7){
+                    $("#indicator").removeClass()
+                    $("#indicator").addClass("indicator indicator-warning");
+                }
+            })
+
+            // To append
+
+            $("#place-name").text(response.name);
+            $("#icon").append(weatherIcon);
 
 
-
-                //$.ajax -> Forecast
-            });
         });
     });
 });
